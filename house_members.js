36aadd3226cf56c16cfa30cc4fb21731cf063dd7
b-s1 <35443416+b-s1@users.noreply.github.com>
2019-03-05 18:27:30 -0500

    fully functioning

--- house_members.js
+++ house_members.js
@@ -4,7 +4,7 @@ module.exports = function(){
 
     /* get people to populate in dropdown */
     function getCharacters(res, mysql, context, complete){
-        mysql.pool.query("SELECT char_id AS character_id, first_name AS fname, last_name AS lname FROm GoT_Character", function(error, results, fields){
+        mysql.pool.query("SELECT char_id, first_name, last_name FROm GoT_Character", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -14,8 +14,6 @@ module.exports = function(){
         });
     }
 
-
-    
     /* get certificates to populate in dropdown */
     function getHouses(res, mysql, context, complete){
         sql = "SELECT house_id, house_name FROM Houses";
@@ -24,19 +22,17 @@ module.exports = function(){
                 res.write(JSON.stringify(error));
                 res.end()
             }
-            context.Houses = results
+            context.manyhouses = results
             complete();
         });
     }
     
-
-
     /* get people with their certificates */
     /* TODO: get multiple certificates in a single column and group on
      * fname+lname or id column
      */
     function getCharactersWithHouses(res, mysql, context, complete){
-        sql = "SELECT character_id, Houses.house_id, CONCAT(first_name,' ',last_name) AS full_name, Houses.house_name FROM GoT_Character INNER JOIN GoT_House_Members ON GoT_Character.char_id = GoT_House_Members.character_id INNER JOIN Houses ON Houses.house_id = GoT_House_Members.house_id ORDER BY house_name, full_name"
+        sql = "SELECT char_id, Houses.house_id, CONCAT(first_name,' ',last_name) AS name, Houses.house_name FROM GoT_Character INNER JOIN GoT_House_Members on GoT_Character.char_id = GoT_House_Members.character_id INNER JOIN Houses on Houses.house_id = GoT_House_Members.house_id ORDER BY name, house_name"
          mysql.pool.query(sql, function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
@@ -69,20 +65,19 @@ module.exports = function(){
         }
     });
 
-
-    /* Associate house(s) with a character and 
-     * then redirect to the characters_with_houses page after adding 
+    /* Associate certificate or certificates with a person and 
+     * then redirect to the people_with_certs page after adding 
      */
     router.post('/', function(req, res){
-        console.log("We get the multi-select houses dropdown as ", req.body.homes)
+        console.log("We get the multi-select houses dropdown as ", req.body.huts)
         var mysql = req.app.get('mysql');
         // let's get out the certificates from the array that was submitted by the form 
-        var houses = req.body.homes
-        var character = req.body.character_id
-        for (let house of houses) {
-          console.log("Processing house id " + house)
+        var manyhouses = req.body.huts
+        var onecharacter = req.body.char_id
+        for (let home of manyhouses) {
+          console.log("Processing house id " + home)
           var sql = "INSERT INTO GoT_House_Members (character_id, house_id) VALUES (?,?)";
-          var inserts = [character, house];
+          var inserts = [onecharacter, home];
           sql = mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
                 //TODO: send error messages to frontend as the following doesn't work
@@ -97,7 +92,7 @@ module.exports = function(){
         res.redirect('/house_members');
     });
 
-    /* Delete a person's certification record */
+    /* Delete a character's house record */
     /* This route will accept a HTTP DELETE request in the form
      * /pid/{{pid}}/cert/{{cid}} -- which is sent by the AJAX form 
      */
