1178e859a0290dc08e40a69e54a1cc9f3ccb0310
b-s1 <35443416+b-s1@users.noreply.github.com>
2019-03-04 19:02:36 -0500

    fixed two sql typos

--- progress/characters.js
+++ progress/characters.js
@@ -4,7 +4,7 @@ module.exports = function(){
 
 
  function getLocations(res, mysql, context, complete){
-        mysql.pool.query("SELECT loc_id as id, loc_name AS name, loc_type AS type FROM Houses", function(error, results, fields){
+        mysql.pool.query("SELECT loc_id as id, loc_name AS name, loc_type AS type FROM GoT_Locations", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -16,7 +16,7 @@ module.exports = function(){
 
 
     function getCharacters(res, mysql, context, complete){
-        mysql.pool.query("SELECT GoT_Character.char_id AS id, first_name AS fname, last_name AS lname, life_status.status_id AS life_status, L1.loc_id AS homeland, L2.loc_id AS current_location FROM GoT_Character c INNER JOIN life_status ON c.life_status = life_status.status_id INNER JOIN GoT_Locations L1 ON c.homeland = L1.loc_id INNER JOIN GoT_Locations L2 ON c.current_location = L2.loc_id ", function(error, results, fields){
+        mysql.pool.query("SELECT char_id AS id, first_name AS fname, last_name AS lname, life_status.status_id AS life_status, L1.loc_id AS homeland, L2.loc_id AS current_location FROM GoT_Character c INNER JOIN life_status ON c.life_status = life_status.status_id INNER JOIN GoT_Locations L1 ON c.homeland = L1.loc_id INNER JOIN GoT_Locations L2 ON c.current_location = L2.loc_id ", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
