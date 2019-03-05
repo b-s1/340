5c14fa208d70ab438a3c775ce19d6ffe94285652
b-s1 <35443416+b-s1@users.noreply.github.com>
2019-03-05 00:51:51 -0500

    add function
    function included for house_members table

--- public/deletecharacter.js
+++ public/deletecharacter.js
@@ -7,3 +7,18 @@ function deleteCharacters(id){
         }
     })
 };
+
+function deleteCharacterHouse(character_id, house_id){
+    $.ajax({
+        url: '/house_members/character_id/' + character_id + '/house_id/' + house_id,
+        type: 'DELETE',
+        success: function(result){
+            if(result.responseText != undefined){
+              alert(result.responseText)
+            }
+            else {
+              window.location.reload(true)
+            } 
+        }
+    })
+  };
