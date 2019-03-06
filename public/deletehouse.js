9be43603697e61f64204b1227b08a01fa6e9f231
clarissa <clarissa.gasiciel@gmail.com>
2019-03-06 17:03:30 -0600

    Updated backgrounds for life status and house locations

--- public/deletehouse.js
+++ public/deletehouse.js
@@ -7,19 +7,3 @@ function deleteHouse(id){
         }
     })
 };
-
-
-function deleteCharacterHouse(character_id, house_id){
-    $.ajax({
-        url: '/house_members/character_id/' + character_id + '/house_id/' + house_id,
-        type: 'DELETE',
-        success: function(result){
-            if(result.responseText != undefined){
-              alert(result.responseText)
-            }
-            else {
-              window.location.reload(true)
-            }
-        }
-    })
-  };
