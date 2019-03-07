948cd8ece9419512877243b60c0d20a0415500ad
clarissa <clarissa.gasiciel@gmail.com>
2019-03-06 21:01:53 -0600

    Formatted tables in div

--- houses.js
+++ houses.js
@@ -175,6 +175,7 @@ module.exports = function(){
 
     /* Route to delete a person, simply returns a 202 upon success. Ajax will handle this. */
     router.delete('/:id', function(req, res){
+      console.log(req.params.id)
         var mysql = req.app.get('mysql');
         var sql = "DELETE FROM House WHERE house_id = ?";
         var inserts = [req.params.id];
@@ -182,6 +183,7 @@ module.exports = function(){
             if(error){
                 res.write(JSON.stringify(error));
                 res.status(400);
+                console.log("Delete unsuccessful");
                 res.end();
             }else{
                 res.status(202).end();
